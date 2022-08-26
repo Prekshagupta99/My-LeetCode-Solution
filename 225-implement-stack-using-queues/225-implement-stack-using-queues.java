cb35b79de22566deab02baa1549ee555a98d8846
Prekshagupta99 <93032512+Prekshagupta99@users.noreply.github.com>
2022-08-26 14:09:03 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (47.84%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,24 +1,64 @@
 class MyStack {
-    Queue<Integer> q1 = new LinkedList<>();
-    Queue<Integer> q2 = new LinkedList<>();
-
+Queue<Integer> first;
+    Queue<Integer> second;
+    public MyStack() {
+        first=new LinkedList<Integer>();
+        second=new LinkedList<Integer>();
+    }
+    
     public void push(int x) {
-        q1.offer(x);
+        first.add(x); 
     }
-
+    
     public int pop() {
-        while (q1.size() != 1) q2.offer(q1.poll());
-        var tmp = q1; q1 = q2; q2 = tmp; //swapping queues
-        return q2.poll();
+         if(first.size()==0)
+            return -1;
+        int x=0;
+      //   System.out.print(first);
+        while(first.size()!=1)
+        { second.add(first.peek());
+        first.remove();
+        }
+        x=first.remove();
+       Queue<Integer> q=first; 
+        first=second;
+        second=q;
+       //System.out.print(second);
+        return x; 
     }
-
+    
     public int top() {
-        var val = pop();
-        push(val);
-        return val;
+        if(first.size()==0)
+            return -1;
+        int x=0;
+      //   System.out.print(first);
+        while(first.size()!=1)
+        { second.add(first.peek());
+        first.remove();
+        }
+        x=first.peek();
+        first.remove();
+        second.add(x);
+       Queue<Integer> q=first; 
+        first=second;
+        second=q;
+       //System.out.print(second);
+        return x;
     }
-
+    
     public boolean empty() {
-        return q1.isEmpty() && q2.isEmpty();
+          if(first.isEmpty())
+            return true;
+        else
+            return false;
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
