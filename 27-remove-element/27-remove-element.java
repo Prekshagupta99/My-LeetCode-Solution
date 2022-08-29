670730648db6b31f235a804a4851164a4a015afd
Prekshagupta99 <93032512+Prekshagupta99@users.noreply.github.com>
2022-08-29 21:49:38 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (17.93%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,14 +1,13 @@
 class Solution {
 public int removeElement(int[] nums, int val) {
-// Counter for keeping track of elements other than val
-int count = 0;
-// Loop through all the elements of the array
-for (int i = 0; i < nums.length; i++) {
-// If the element is not val
-if (nums[i] != val) {
-nums[count++] = nums[i];
-}
-}
-return count;
-}
+         int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                nums[c] = nums[i];
+                c++;
+                
+            }
+        }
+        return c;
+    }
 }
